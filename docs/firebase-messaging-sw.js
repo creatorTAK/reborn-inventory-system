eb8e4393f271451c105ae1107858543ee5e52603
Takuji <mercari.yasuhirotakuji@gmail.com>
2025-10-17 11:47:15 +0900

    fix: Firebase SDK を10.8.0に更新 - API key not valid エラー解決

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -8,8 +8,8 @@ const CACHE_NAME = 'reborn-pwa-' + CACHE_VERSION;
 // 通知の重複を防ぐためのキャッシュ
 const notificationCache = new Set();
 
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
 // Firebase設定
 const firebaseConfig = {
